36b9ae0f1cbb807c10023946693548d7b9ce2957
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-19 15:24:00 +0300

    Show the account activation status

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,12 @@
     </div>
 @endif
 
+    @if(Auth::user()->verified == 0)
+        <div class="alert alert-danger">
+            <h3 class="text-center">Your account is not activated. Go to your email to activate your account.</h3>
+        </div>
+    @endif
+
     <div class="container">
         <!--first row-->
         <div class="row banner">
