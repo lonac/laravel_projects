27cae3f34e927f5c324a3eab0e64b453966fddb2
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-20 00:36:18 +0300

    Create email model and migration

---
+++ database/migrations/2016_10_19_213150_create_emails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('church_id')->unsigned();
+            $table->foreign('church_id')->references('id')->on('churches')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
