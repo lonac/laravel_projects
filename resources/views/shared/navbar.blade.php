a5ad613ea0690d237d119a10b6afe14cf1b8c8cf
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-19 15:35:21 +0300

    Fix the links

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -8,16 +8,16 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><b>Search MyChurch</b></a>
+            <a class="navbar-brand" href="{{ url('/') }}"><b>Search MyChurch</b></a>
         </div>
 
         <!-- Navbar Right -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-                <li><a href="/event">Events</a></li>
+                <li class="active"><a href="{{ url('home') }}">Home</a></li>
+                <li><a href="{{ url('about') }}">About</a></li>
+                <li><a href="{{ url('contact') }}">Contact</a></li>
+                <li><a href="{{ url('events') }}">Events</a></li>
                 <!-- Authentication Links -->
                 @if (Auth::guest())
                     <li><a href="{{ url('/users/login') }}">Login</a></li>
