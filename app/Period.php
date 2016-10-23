9e08e3f5cc565b2bdd9fa93b0e7ccba3174480f6
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-23 23:28:39 +0300

    Create Period model and migration

---
+++ app/Period.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Period extends Model
+{
+    protected $fillable = [
+    	'church_id',
+    	'title',
+    	'slug',
+    	'description',
+    	'from',
+    	'to',
+    	'published',
+    	'day_id',
+    ];
+}
---
+++ database/migrations/2016_10_23_201434_create_periods_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeriodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('periods', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('church_id')->unsigned();
+            $table->foreign('church_id')->references('id')->on('churches')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description')->nullable();
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('finish_time')->nullable();
+            $table->boolean('published')->default(false);
+
+            $table->integer('day_id')->unsigned();
+            $table->foreign('day_id')->references('id')->on('days')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('periods');
+    }
+}
