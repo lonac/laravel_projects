620c1782d267976669ab5a42ecb4fad38d2a562a
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-23 20:26:09 +0300

    Display church info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,14 +18,52 @@
 
     <div class="container">
         <!--first row-->
-        <div class="row banner">
+        <div class="row">
+            <div class="col-sm-9">
             <div class="panel panel-success">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Panel title</h3>
+                    <h3 class="panel-title">My church</h3>
                 </div>
                 <div class="panel-body">
                     @if($church->count() > 0)
-
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <tbody>
+                                    <tr>
+                                        <th>Name</th>
+                                        <td>{{ $church->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Category</th>
+                                        <td>{{ $church->category->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>About</th>
+                                        <td>{{ $church->description }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Region</th>
+                                        <td>{{ $church->region->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>District</th>
+                                        <td>{{ $church->district->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Address</th>
+                                        <td>{{ $church->address }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Added</th>
+                                        <td>{{ $church->created_at->diffForHumans() }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Updated</th>
+                                        <td>{{ $church->updated_at->diffForHumans() }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     @else
                         <div class="jumbotron text-center">
                             <h2>You have no any church</h2>
@@ -35,6 +73,28 @@
                         </div>
                     @endif
                 </div>
+                <div class="panel-footer">
+                    
+                </div>
+            </div>                
+            </div>
+            <div class="col-sm-3">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Contact info</h3>
+                    </div>
+                    <div class="panel-body">
+                        No contact info
+                    </div>
+                </div>
+                <div class="panel panel-warning">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Events</h3>
+                    </div>
+                    <div class="panel-body">
+                        No event
+                    </div>
+                </div>                
             </div>
         </div>
 
