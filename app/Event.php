3c942993f40da2125da8afbeece231ca3d07d846
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-24 08:50:05 +0300

    Create Event model and migration

---
+++ app/Event.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'church_id',
+        'title',
+        'slug',
+        'description',
+        'time',
+    ];
+}
---
+++ database/migrations/2016_10_23_215332_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('church_id')->unsigned();
+            $table->foreign('church_id')->references('id')->on('churches')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->timestamp('time')->default(null);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
