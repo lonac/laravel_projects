b79efb18406ba540c3e4adee46939f274986ed6a
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-24 10:00:45 +0300

    Show events

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ url('/') }}"><b>Search MyChurch</b></a>
+            <a class="navbar-brand" href="{{ url('/') }}">Search MyChurch</a>
         </div>
 
         <!-- Navbar Right -->
