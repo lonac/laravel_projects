f8c47903b931dbde46c2fa31e554eb1eb9e3580d
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-28 13:43:17 +0300

    Beautify

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -28,24 +28,27 @@
                 </div>
             </div>
             <div class="col-xs-12 col-sm-9">
-                @if($churches->count() > 0)
-                    @foreach($churches->chunk(2) as $churchesSet)
-                        <div class="row">
-                            @foreach($churchesSet as $church)
-                                <div class="col-sm-4">
-                                    @include('partials.card', ['church' => $church])
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        @if($churches->count() > 0)
+                            @foreach($churches->chunk(2) as $churchesSet)
+                                <div class="row">
+                                    @foreach($churchesSet as $church)
+                                        <div class="col-sm-4">
+                                            @include('partials.card', ['church' => $church])
+                                        </div>
+                                    @endforeach
                                 </div>
                             @endforeach
+                        @else
+                            <h1 class="text-center">No church</h1>
+                        @endif
+
+                        <div class="text-center">
+                            {{ $churches->links() }}
                         </div>
-                    @endforeach
-                @else
-                    <h1 class="text-center">No church</h1>
-                @endif
-
-                <div class="text-center">
-                    {{ $churches->links() }}
+                    </div>
                 </div>
-
             </div>
         </div>
     </div>
