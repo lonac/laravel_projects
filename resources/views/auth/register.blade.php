2f896d28bf05f20970cdeb02e7b88043dd20df6e
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-10 22:22:38 +0300

    Add headers to login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,33 +2,34 @@
 @section('title','Register')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                     <form class="form-horizontal" role="form" method="POST" >
-                        {{ csrf_field() }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h2>Register for free</h2>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST">
+                            {{ csrf_field() }}
 
-                         @foreach ($errors->all() as $error)
-                         <p class="alert alert-danger">{{ $error }}</p>
-                         @endforeach
+                            @foreach ($errors->all() as $error)
+                                <p class="alert alert-danger">{{ $error }}</p>
+                            @endforeach
 
                             {!! csrf_field() !!}
-                            <fieldset>
-                            <legend><b><font color="red">Register for Free!</font></legend>
-                            Personal details</b>
+
                             <div class="form-group">
                                 <label for="name" class="col-lg-3 control-label">Name</label>
                                 <div class="col-lg-7">
-                                    <input type="text" class="form-control" id="name" placeholder="Name" name="name" value="{{ old('name') }}">
+                                    <input type="text" class="form-control" id="name" placeholder="Name" name="name"
+                                           value="{{ old('name') }}">
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <label for="email" class="col-lg-3 control-label">Email</label>
                                 <div class="col-lg-7">
-                                    <input type="email" class="form-control" id="email" placeholder="Email" name="email" value="{{ old('email') }}">
+                                    <input type="email" class="form-control" id="email" placeholder="Email" name="email"
+                                           value="{{ old('email') }}">
                                 </div>
                             </div>
 
@@ -42,7 +43,8 @@
                             <div class="form-group">
                                 <label for="password" class="col-lg-3 control-label">Confirm password</label>
                                 <div class="col-lg-7">
-                                    <input type="password" class="form-control" placeholder="*****" name="password_confirmation">
+                                    <input type="password" class="form-control" placeholder="*****"
+                                           name="password_confirmation">
                                 </div>
                             </div>
 
@@ -52,11 +54,11 @@
                                     <button type="submit" class="btn btn-primary">Register</button>
                                 </div>
                             </div>
-                        </fieldset>    
-                    </form>
+                            </fieldset>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
